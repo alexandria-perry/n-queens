4ee91f9d1e6f896a3475b92dbe84cd80881d0e65
alexandria-perry <aeperry22@gmail.com>
2018-11-15 11:29:24 -0800

    finished NRooksSolutions.

--- src/solvers.js
+++ src/solvers.js
@@ -13,72 +13,77 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
-
-window.findNRooksSolution = function(num) {
-  if (!board) {
-    var board = new Board({ n: num});
+function objCopy(input){
+  var rows = Object.keys(input.rows()).length;
+    //copies current object to create a copy of the new board as the current
+    //togglePiece function toggles all pieces within the "all boards" object.
+  var result = [];
+  for (var i = 0; i < rows; i++){
+    result.push(_.clone(input.rows()[i]));
   }
-  for (var i = 0; i < num; i++) {
-    for (var j = 0; j < num; j++) {
-      board.togglePiece(i, j);
-      if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()) {
-        board.togglePiece(i, j);
-      }
+  return result;
+}
+
+
+function factorial(n){
+  var value = 1;
+    for (var i = 1; i < n ; i++){
+      value = value * i;
+    }
+  return value;
+}
+
+var rook = function(num){
+  var board = new Board({n:num});
+  for(var i = 0; i < num; i++){
+    var random = Math.floor(Math.random()*num);
+    board.togglePiece(i,random);
+    if(board.hasAnyRooksConflicts()){
+      board.togglePiece(i, random);
+      i--;
+    }else{
+    
     }
   }
-  //find index of piece
-  //use row/col check functions
-  //
-  
-  
-  var solution = board.rows();
-
-  // console.log('Single solution for ' + num + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return board;
+}
+window.findNRooksSolution = function(num) {
+  var board = rook(num);
+  //console.log('Single solution for ' + num + ' rooks:', JSON.stringify(board.rows()));
+  return board.rows();
 };
-
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 // try and formulate a recursive function
 window.countNRooksSolutions = function(num) {
   var boardObj = {};
   var count = 0;
-  //factoral function to account for total possibilities of num X num matrix
-  // var boundary = 1;
-  // for (var i = 1; i < num ; i++){
-  // 	boundary = boundary * i;
-  // }
-  var objCopy = function(input){
-    var rows = Object.keys(input).length;
-    var columns = input.rows()[0].length;
-    //copies current object to create a copy of the new board as the current
-    //togglePiece function toggles all pieces within the "all boards" object.
-    var result = [];
-  }
+  var testArray = [];
   for(var i = 0; i < num; i++){
     boardObj[count] = new Board({ n: num});
     boardObj[count].togglePiece(0, i);
 	count++;
   }
-  var rookBoard = function(num,j){
-    var objLen = Object.keys(boardObj).length;
-    var testObj;
-    for (key in boardObj){
-      testObj = boardObj[key];
-      for (var i = 0; i < num; i++){
+
+var rookBoard = function (num,j){
+  var objLen = Object.keys(boardObj).length;
+  var testObj;
+  for (key in boardObj){
+    testObj = new Board(objCopy(boardObj[key]));
+    for (var i = 0; i < num; i++){
+      testObj.togglePiece(j,i);
+      if(testObj.hasAnyRooksConflicts()) {
         testObj.togglePiece(j,i);
-        if(testObj.hasAnyRooksConflicts()) {
+      }
+      else{
+          boardObj[count] = new Board(objCopy(testObj));
+          count++;
           testObj.togglePiece(j,i);
-        }
-        else{
-            boardObj[count] = new Board(testObj.rows());
-        }
       }
     }
   }
-  
-  for (var i = 0; i < num; i++){
+}
+  for (var i = 1; i < num; i++){
     rookBoard(num, i);
     for (key1 in boardObj){
       var compare = JSON.stringify(boardObj[key1].rows());
@@ -91,23 +96,20 @@ window.countNRooksSolutions = function(num) {
         }
       }
     }
-    for(key in boardObj){
-      console.log(Object.keys(boardObj), count);
+  }
+  
+  for (key in boardObj){
+    testArray = [];
+    for (var i = 0; i < num; i++){
+      testArray.push(boardObj[key].rows()[i]);
+    }
+    testArray = _.flatten(testArray);
+    testArray = _.compact(testArray);
+    if (testArray.length !== num) {
+      delete boardObj[key];
     }
   }
-  // for (key1 in boardObj){
-  // 	var compare = JSON.stringify(boardObj[key1].rows());
-  // 	for (key2 in boardObj) {
-  // 		if (key1 === key2){
-  // 		}else{
-  // 			if(compare === JSON.stringify(boardObj[key2].rows())){
-  // 				delete boardObj[key2];
-  // 			}
-  // 		}
-  // 	} 
-  // }
   var solutionCount = Object.keys(boardObj).length;
-
   return solutionCount;
 };
 
